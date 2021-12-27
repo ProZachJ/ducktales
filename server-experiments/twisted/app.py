e081fddf4f4d5e6442f640b22f69b95db872ee4a
Zach Jones <zach.jones@whitehatsec.com>
2021-12-27 14:30:09 -0600

    cleanup

---
+++ server-experiments/twisted/app.py
@@ -0,0 +1,15 @@
+from twisted.web import server, resource
+from twisted.internet import reactor, endpoints
+
+class Counter(resource.Resource):
+    isLeaf = True
+    numberRequests = 0
+
+    def render_GET(self, request):
+        self.numberRequests += 1
+        request.setHeader(b"content-type", b"text/plain")
+        content = u"I am request #{}\n".format(self.numberRequests)
+        return content.encode("ascii")
+
+endpoints.serverFromString(reactor, "tcp:8080").listen(server.Site(Counter()))
+reactor.run()
